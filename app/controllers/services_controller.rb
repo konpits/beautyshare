561726cdf970b159fd1eb6dd497163a570b735b0
konpits <31943872+konpits@users.noreply.github.com>
2018-05-07 18:37:37 +1000

    edit service show and index

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -10,6 +10,8 @@ class ServicesController < ApplicationController
   # GET /services/1
   # GET /services/1.json
   def show
+    user = @service.user
+    @name  =  Profile.find(user.id).first_name + " " + Profile.find(user.id).last_name 
   end
 
   # GET /services/new
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,8 @@
 class Profile < ApplicationRecord
   belongs_to :user
+
+
+  def full_name user 
+    Profile.find(user.id).first_name + " " + Profile.find(user.id).last_name 
+  end
 end
